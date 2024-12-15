c30d0e069d3f670d1b60f5913788415d9c747886
OlenaMars <elena.marsina@gmail.com>
2024-12-15 12:17:06 -0600

    Update HelloWord.java

--- src/day01/HelloWord.java
+++ src/day01/HelloWord.java
@@ -4,6 +4,11 @@ public class HelloWord {
     public static void main(String[] args) {
 
         System.out.println("Hello Word");
+        System.out.println("Hello Siama");
+        System.out.println("Hello Yevhen");
+        System.out.println("Hello Olena");
+        System.out.println("Hello");
+
 
     }
 
