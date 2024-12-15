2008bd99f69796e3f4e17b8f495c79c5763c26d0
OlenaMars <elena.marsina@gmail.com>
2024-12-15 12:45:13 -0600

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -2,6 +2,8 @@ package day01;
 
 public class HelloCydeo {
     public static void main(String[] args) {
+
+        //add new
         System.out.println("Hola");
          System.out.println("Hi");
          System.out.println("Hello");
